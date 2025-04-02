1500f3e41adb6c2254fafc8513170960d639eec5
natashaash098 <lisinanatasha123@gmail.com>
2025-04-02 19:05:41 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+from PyQt6.QtWidgets import QApplication
+
+from Interface.Class_MainWindow import MainWindow
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec())
